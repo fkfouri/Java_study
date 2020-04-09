dd89ac9116e8dab0e4f0c095f343aebcca19fde3
fkfouri <kfouri.fabio@gmail.com>
2020-04-09 13:11:37 -0300

    Criaçao de Composicao, Herança, Poliformismo, Interface. Finalizacao do Curso.

--- bytebank-heranca-conta/src/ContaCorrente.java
+++ bytebank-heranca-conta/src/ContaCorrente.java
@@ -1,5 +1,5 @@
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 	
 	public ContaCorrente(int agencia, int numero)  {
 		super(agencia, numero);
@@ -13,6 +13,12 @@ public class ContaCorrente extends Conta {
 	public boolean saca(double valor) {
 		double valorASacar = valor + 0.2; // taxa de 20 centavos por saque.
 		return super.saca(valorASacar);
+	}
+
+
+	@Override
+	public double getValorImposto() {
+		return super.getSaldo() * 0.01;
 	}
 
 }
