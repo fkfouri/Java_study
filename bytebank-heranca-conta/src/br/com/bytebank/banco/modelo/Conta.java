d714ceb805ba1e8b1624d300f345c48b99ef8b35
fkfouri <kfouri.fabio@gmail.com>
2020-04-09 20:10:47 -0300

    Javadoc

--- bytebank-heranca-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-heranca-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,8 +1,14 @@
 package br.com.bytebank.banco.modelo;
 
-//isso eh um schema de uma conta
-// eh uma especificacao do que eh uma conta
+/**
+ * Classe que representa a moldura (Schema) de uma conta.
+ * 
+ * @author fkfouri
+ * 
+ */
 public abstract class Conta {
+	//isso eh um schema de uma conta
+	// eh uma especificacao do que eh uma conta
 
 	// 4 caracteristicas // 4 atributos
 	protected double saldo;
@@ -11,7 +17,12 @@ public abstract class Conta {
 	private Cliente titular;
 	private static int total = 0; // um atributo da classe e nao mais do objeto
 
-	// Construtor
+	/**
+	 * Construtor para inicializar o objeto Conta a partir da agencia e numero
+	 * 
+	 * @param agencia
+	 * @param numero
+	 */
 	public Conta(int agencia, int numero) {
 		Conta.total++;
 		this.agencia = agencia;
@@ -22,6 +33,11 @@ public abstract class Conta {
 	// isso eh um metodo, uma maneira de fazer algo
 	public abstract void deposita(double valor);
 
+	/**
+	 * Valor precisa ser maior que o saldo
+	 * 
+	 * @param valor
+	 */
 	public void saca(double valor) {
 		if (this.saldo < valor) {
 			throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
